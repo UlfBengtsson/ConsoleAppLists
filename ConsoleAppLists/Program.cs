0977d222d088213eadb43ba604c8ee452655959b
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-10-30 11:17:09 +0100

    brake

--- ConsoleAppLists/Program.cs
+++ ConsoleAppLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleAppLists
 {
@@ -6,14 +7,95 @@ namespace ConsoleAppLists
     {
         static void Main(string[] args)
         {
-            ArrayEx1();
-            ArrayEx2();
-            ArrayEx3();
-            ArrayEx4();
+            //ArrayEx1();
+            //ArrayEx2();
+            //ArrayEx3();
+            //ArrayEx4();
+
+            //ListEx1();
+            //ListEx2();
+
+            //DictEx1();
 
         }//main
 
+        static void DictEx1()
+        {
+            Dictionary<String, double> favNumbers = new Dictionary<string, double>();
 
+            favNumbers.Add("Lucky", 17);
+            favNumbers.Add("Pi", 3.14);
+
+            //same as
+            /*
+            Dictionary<String, double> favNumbers = new Dictionary<string, double>
+            {
+                { "Lucky", 17 },
+                { "Pi", 3.14 }
+            };
+            */
+
+            Console.WriteLine(favNumbers.GetValueOrDefault("Pi"));
+            Console.WriteLine(favNumbers.GetValueOrDefault("PI"));
+
+            double result;
+            //bool okay = favNumbers.TryGetValue("Lucky", out result);
+            bool okay = favNumbers.TryGetValue("lucky", out result);
+
+            if (okay)
+            {
+                Console.WriteLine("Results in: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid key");
+            }
+        }
+
+        static void ListEx1()
+        {
+            List<string> cars = new List<string>();
+
+            cars.Add("BMW");
+            cars.Add("Trabant");
+            cars.Add("Porsche");
+            cars.Add("Saab");
+
+            int indexOfSaab = cars.IndexOf("Saab");
+
+            Console.WriteLine("Index: " + indexOfSaab);
+            Console.WriteLine(cars[indexOfSaab] + "\n-----------------------");
+
+            foreach (String aCar in cars)
+            {
+                Console.WriteLine(aCar);
+            }
+
+            cars.Remove("Trabant");
+            Console.WriteLine("Removed Trabant\n");
+
+            foreach (String aCar in cars)
+            {
+                Console.WriteLine(aCar);
+            }
+
+        }
+
+        static void ListEx2()
+        {
+            List<int> numbers = new List<int>();
+
+            List<double> doubleList = new List<double>();
+
+            numbers.Add(123);
+            doubleList.Add(123);
+
+            numbers.Add( (int)23.1 );// type cast - double - tripple cheack before you do it.
+
+            doubleList.Add(23.2);
+
+            //doubleList.AddRange(numbers); No, will not work
+        }
 
         static void ArrayEx1()
         {
